a740f85efb7875c34065f78f7824906e9db725da
bfren <ben@bcgdesign.com>
2021-12-28 12:27:01 +0000

    v3.3.1 (#67)
    Minor updates
    * Adding generate-dockerfiles.sh
    * Using latest base images

---
+++ generate-dockerfiles.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+docker pull bfren/alpine
+
+BASE_REVISION="3.2.1"
+echo "Base: ${BASE_REVISION}"
+
+PHP_VERSIONS="7.3 7.4 8.0"
+for V in ${PHP_VERSIONS} ; do
+
+    echo "WordPress for PHP ${V}"
+
+    DOCKERFILE=$(docker run \
+        -v ${PWD}:/ws \
+        bfren/alpine esh \
+        "/ws/Dockerfile.esh" \
+        BASE_REVISION=${BASE_REVISION} \
+        PHP_MINOR=${V}
+    )
+
+    echo "${DOCKERFILE}" > ./php${V}/Dockerfile
+
+done
+
+echo "Done."
