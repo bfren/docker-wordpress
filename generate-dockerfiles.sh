662ad653109636d04efeed5f8a69495a187693e0
bfren <ben@bcgdesign.com>
2022-06-11 10:31:12 +0100

    v4.4.2 (#109)
    Minor updates
    * Using latest base images
    * Updating to PHP 8.0.20
    * Adding PHP 8.1 build

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_REVISION="5.0.1"
+BASE_REVISION="5.1.1"
 echo "Base: ${BASE_REVISION}"
 
-PHP_VERSIONS="7.3 7.4 8.0 8.1-edge"
+PHP_VERSIONS="7.3 7.4 8.0 8.1"
 for V in ${PHP_VERSIONS} ; do
 
     echo "FreeScout for PHP ${V}"
