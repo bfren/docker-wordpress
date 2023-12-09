bc83ea5e3ee31fd78bbf1df59cf87625fee772c2
bfren <bf@bfren.dev>
2023-12-09 11:34:04 +0000

    Adding PHP 8.3

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_VERSION="7.0.5"
+BASE_VERSION="7.1.0"
 echo "Base: ${BASE_VERSION}"
 
-PHP_VERSIONS="7.4 8.0 8.1 8.2"
+PHP_VERSIONS="7.4 8.0 8.1 8.2 8.3"
 for V in ${PHP_VERSIONS} ; do
 
     echo "WordPress for PHP ${V}"
