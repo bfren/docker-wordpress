32ad23d73ec5611c4408afdd1642dbe9c755842f
bfren <bf@bfren.dev>
2024-02-23 20:09:42 +0000

    Adding new test framework

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+IMAGE=wordpress
+VERSION=`cat VERSION`
+PHP=${1:-8.3}
+TAG=${IMAGE}-test
+
+docker buildx build \
+    --load \
+    --build-arg BF_IMAGE=${IMAGE} \
+    --build-arg BF_VERSION=${VERSION} \
+    -f php${PHP}/Dockerfile \
+    -t ${TAG} \
+    . \
+    && \
+    docker run --entrypoint "/usr/bin/env" ${TAG} -i nu -c "use bf test ; test"
