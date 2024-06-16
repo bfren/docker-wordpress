d61cfed19155c6f930ac1e7130738c0064a43960
bfren <bf@bfren.dev>
2024-06-16 09:35:40 +0100

    v6.3.2 (#230)
    Minor updates
    * Updating to PHP 8.1.29
    * Adding .empty overlay
    
    Build updates
    * Using specific PHP image versions

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,13 +4,14 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_VERSION="7.2.7"
+BASE_VERSION="7.2.9"
 echo "Base: ${BASE_VERSION}"
 
 PHP_VERSIONS="7.4 8.0 8.1 8.2 8.3"
 for V in ${PHP_VERSIONS} ; do
 
     echo "WordPress for PHP ${V}"
+    PHP_REVISION=`cat php${V}/PHP_REVISION`
 
     DOCKERFILE=$(docker run \
         -v ${PWD}:/ws \
@@ -18,7 +19,8 @@ for V in ${PHP_VERSIONS} ; do
         bfren/alpine esh \
         "/ws/Dockerfile.esh" \
         BASE_VERSION=${BASE_VERSION} \
-        PHP_MINOR=${V}
+        PHP_MINOR=${V} \
+        PHP_REVISION=${PHP_REVISION}
     )
 
     echo "${DOCKERFILE}" > ./php${V}/Dockerfile
