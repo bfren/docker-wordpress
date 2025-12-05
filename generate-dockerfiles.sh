8a858a28974507bcfd97155aa3e3c336b77ec5f4
bfren <bf@bfren.dev>
2025-12-05 21:15:17 +0000

    Using latest base images

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_VERSION="8.0.1"
+BASE_VERSION="8.1.0"
 echo "Base: ${BASE_VERSION}"
 
-PHP_VERSIONS="7.4 8.0 8.1 8.2 8.3 8.4"
+PHP_VERSIONS="7.4 8.0 8.1 8.2 8.3 8.4 8.5"
 for V in ${PHP_VERSIONS} ; do
 
     echo "WordPress for PHP ${V}"
